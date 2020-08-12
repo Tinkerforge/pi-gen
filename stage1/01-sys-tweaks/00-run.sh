64b47e83e2cac0eebe45139d6d749f76dc682982
Matthias Bolte <matthias@tinkerforge.com>
2020-08-12 15:52:18 +0200

    Prepare tng account the way initramfs expects it
    Use the expetced default password and lock the account.

--- stage1/01-sys-tweaks/00-run.sh
+++ stage1/01-sys-tweaks/00-run.sh
@@ -9,6 +9,7 @@ if ! id -u ${FIRST_USER_NAME} >/dev/null 2>&1; then
 	adduser --disabled-password --gecos "" ${FIRST_USER_NAME}
 fi
 echo "${FIRST_USER_NAME}:${FIRST_USER_PASS}" | chpasswd
+passwd -l ${FIRST_USER_NAME}
 echo "root:root" | chpasswd
 EOF
 
